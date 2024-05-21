07b1375653d067d967223806276c35501add7a0b
HexMerlin <alec-sweden@hotmail.com>
2024-05-21 14:33:15 +0200

    Add files

---
+++ Assets/Scripts/Calc/Rational.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+
+
+public enum Separator
+{
+    None = 1,
+    RadixPoint = -1,
+    RepetendBegin = -2,
+    RadixPointAndRepetendBegin = -3,
+}
+
+public partial struct Rational : IEquatable<Rational>, IComparable<Rational>
+{
+    public readonly BigInteger Numerator { get; }
+    public readonly BigInteger Denominator { get; }
+
+    private int computedLength;
+
+    private int computedPeriod;
+      
+
+    public static Rational Invalid => new Rational(0, 0, false);
+    public static Rational Zero => new Rational(0, 1, false);
+    public static Rational One => new Rational(1, 1, false);
+
+    public static Rational Half => new Rational(1, 2, false);
+
+    public bool IsSpecialDelimiter => Denominator < 0;
+    public bool IsInvalid => Denominator == 0;
+
+
+    private const int uninitializedInt = int.MinValue + 1;
+
+ 
+    //public static implicit operator Rational(Delimiter type)
+    //    => new Rational(0, (int)type, false);
+
+    //public Separator Separator => Denominator > BigInteger.Zero
+    //   ? Separator.None
+    //   : Enum.IsDefined(typeof(Separator), (int)Denominator)
+    //       ? (Separator)(int)Denominator
+    //       : throw new ArgumentOutOfRangeException(nameof(Denominator), "Invalid denominator value");
+
+
+    //public Rational() : this(0, 1, false) { }   //use when there is support for C# 10 
+
+    public Rational(BigInteger numerator, BigInteger denominator) : this(numerator, denominator, true) { }
+
+    private Rational(BigInteger numerator, BigInteger denominator, bool checkAndNormalize)
+    {
+        computedLength = uninitializedInt;
+        computedPeriod = uninitializedInt;
+
+        if (checkAndNormalize)
+        {
+            if (denominator <= 0)
+            {
+                Numerator = 0;
+                Denominator = 0;
+                return;
+            }
+
+            if (denominator.Sign == -1)
+            {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+
+            BigInteger gcd = BigInteger.GreatestCommonDivisor(BigInteger.Abs(numerator), BigInteger.Abs(denominator));
+            if (gcd > 1)
+            {
+                numerator /= gcd;
+                denominator /= gcd;
+            }
+        }
+        this.Numerator = numerator;
+        this.Denominator = denominator;
+    }
+
+
+    public Rational(BigInteger numerator) : this(numerator, 1, false) { }
+
+    public Rational(string input)
+    {
+        computedLength = uninitializedInt;
+        computedPeriod = uninitializedInt;
+        Denominator = BigInteger.One;
+
+        int pointIndex = input.IndexOf('.');
+        if (pointIndex != -1) //not currently supporting point notation
+        {
+            Numerator = 0;
+            Denominator = 0;
+            return;
+            //denominator <<= (input.Length - pointIndex - 1); //compute the denominator
+            //input = input.Remove(pointIndex, 1); // remove the point
+        }
+
+
+        Numerator = BigInteger.Parse(input);
+
+    }
+
+    private void ComputeLengthAndPeriod()
+    {
+        if (computedPeriod != uninitializedInt)
+            return;
+
+        computedLength = 0;
+        computedPeriod = 0;
+        foreach ((_, Separator separator) in RotationsBin)
+        {
+            if (computedPeriod >= 1)
+                computedPeriod++;
+            else if (separator == Separator.RepetendBegin || separator == Separator.RadixPointAndRepetendBegin)
+                computedPeriod = 1;
+               
+            computedLength++;
+        }
+
+    }
+
+    public int Length
+    {
+        get
+        {
+            ComputeLengthAndPeriod();
+            return computedLength;
+        }
+    }
+
+    public int Period
+    {
+        get
+        {
+            ComputeLengthAndPeriod();
+            return computedPeriod;
+        }
+    }
+        
+
+    public BigInteger IntegerPart => Numerator / Denominator;
+
+    public Rational FractionalPart => this - IntegerPart;
+
+    public int IntegerLength => (int)BigInteger.Abs(IntegerPart).GetBitLength();
+
+    public Rational this[int index] => (this << (index - IntegerLength)).FractionalPart;
+
+    public Rational Weight(int index) => One << (IntegerLength - index - 1);
+
+    public Rational Term(int index)
+    {
+        int repetendStart = Length - Period;
+        bool bit = this[index] >= Half;
+
+        if (index < repetendStart)
+            return bit ? Weight(index) : Zero;
+        else
+            return bit ? (Weight(index) << Period) / ((BigInteger.One << Period) - BigInteger.One) : Rational.Zero;
+        
+         
+    }
+   
+    public Rational DivideByMersenneCeiling()
+    {
+        BigInteger div = MersenneCeiling(Numerator);
+        return this / div;
+        
+    }
+
+
+    public static BigInteger MersenneCeiling(BigInteger num)
+    {
+        num = BigInteger.Abs(num);
+        BigInteger ceiling = 1;
+
+        while (ceiling <= num)
+            ceiling = (ceiling << 1) + 1;
+
+        return ceiling;
+    }
+
+
+
+    public Rational WeightFloor
+    {
+        get
+        {
+            Rational w = Rational.One;
+            while (w < this.Abs)
+                w <<= 1;
+            return w >> 1;
+        }
+    }
+
+
+    public IEnumerable<(Rational, Separator)> RotationsBalBin
+    {
+        get
+        {
+            yield break;
+            //Rational r = this;
+            //Rational w = WeightFloor;
+
+            //while (w >= 1)
+            //{
+            //    yield return r;
+            //    if (r > 0)
+            //        r -= w;
+            //    else
+            //        r += w;
+
+            //    w >>= 1;
+            //}
+            //Debug.Assert(w == Half);
+            //static bool IsDoubleOdd(Rational r) => !r.Numerator.IsEven && !r.Denominator.IsEven; //used to determine when the repetend starts
+            //Rational repetendStart = IsDoubleOdd(r) ? r : Invalid; //used for bookkeeping when the repetend started
+
+            //if (repetendStart.IsInvalid)
+            //    yield return Delimiter.RadixPoint; //no repetend detected yet, return only the radix point
+            //else
+            //    yield return Delimiter.RadixPointAndRepetendBegin; //repetend detected, return the combined radix point and the repetend begin
+
+            //while (true)
+            //{
+            //    if (r.IsZero)
+            //        yield break;
+            //    if (repetendStart.IsInvalid && IsDoubleOdd(r))
+            //    {
+            //        repetendStart = r;
+            //        yield return Delimiter.RepetendBegin;
+            //    }
+
+            //    yield return r;
+
+            //    r <<= 1;
+
+            //    if (r > 0)
+            //        r--;
+            //    else
+            //        r++;
+
+            //    if (r == repetendStart)
+            //        yield break;
+            //}
+
+        }
+    }
+   
+    public IEnumerable<(Rational rational, Separator separator)> RotationsBin
+    {
+        get
+        {
+            if (this < 0) yield break;
+            Rational firstInRepetend = Invalid;
+            
+            int integerLength = this.IntegerLength;
+            
+            for (int len = 0; ; len++)
+            {
+                Rational r = this[len];
+
+                if (r == firstInRepetend || r.IsZero)
+                   break;
+                bool emitRadixPoint = len == integerLength;
+                bool emitRepetendBegin = firstInRepetend.IsInvalid && r.Denominator.IsOdd();
+                Separator separator = emitRepetendBegin && emitRadixPoint ? Separator.RadixPointAndRepetendBegin : emitRepetendBegin ? Separator.RepetendBegin : emitRadixPoint ? Separator.RadixPoint : Separator.None;
+                if (emitRepetendBegin)
+                    firstInRepetend = r;
+
+                yield return (r, separator);
+            }
+         
+        }
+    }
+
+    public IEnumerable<Rational> Partition
+    {
+        get
+        {
+            for (int i = 0; i < Length; i++)
+                yield return Term(i);
+        }
+    }
+
+
+    private static char? ToSeparatorChar(Separator separator) => separator switch
+    { 
+        Separator.RadixPoint => '⠄',
+        Separator.RepetendBegin => '⠁',
+        Separator.RadixPointAndRepetendBegin => '⠅',
+        //Separator.RepetendEnd => '…',
+        _ => null
+    };
+
+    public string ToStringNormal() => Denominator == 1 ? Numerator.ToString() : $"{Numerator}/{Denominator}";
+
+
+    public string ToStringPartition() => $"{string.Join(" ", Partition.Select(r => r.ToString()))}";
+
+    public string ToStringBin()
+    {
+        StringBuilder sb = new StringBuilder(); 
+        foreach ((Rational r, Separator s) in RotationsBin)
+        {
+            if (s != Separator.None)
+                sb.Append(ToSeparatorChar(s));
+            sb.Append(r >= Half ? '1' : '0');
+        }
+        return sb.ToString();
+    }
+
+    public string ToStringBalBin() => "Not implemented";
+
+    public string ToStringRotationsBin()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach ((Rational r, Separator s) in RotationsBin)
+        {
+            if (s != Separator.None)
+                sb.Append(ToSeparatorChar(s));
+            sb.Append(r.Denominator == r.Denominator ? (r.Numerator + "/") : r.ToString());
+            sb.Append(' ');
+        }
+        return sb.ToString();
+
+    }
+
+
+    public string ToStringRotationsBalBin() => "Not implemented";
+
+    public string ToStringRepInfo()
+    {
+
+        return $"P={Period}";
+    }
+
+    public override string ToString() => Denominator == 1 ? Numerator.ToString() : $"{Numerator}/{Denominator}";
+
+
+
+}
---
+++ Assets/Scripts/UI/MainViewController.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+[RequireComponent(typeof(UIDocument))]
+public class MainViewController : MonoBehaviour
+{
+    public UIDocument UIDocument;
+
+    private List<NumberEntry> outputEntries;
+
+    private Label input;
+
+    private MultiColumnListView output;
+
+    private VisualElement buttonGrid;
+
+    private readonly Button[] numberFormatButtons = new Button[Enum.GetValues(typeof(Format)).Length];
+
+    private Format numberFormat = Format.Normal;
+    private Format NumberFormat
+    {
+        get => numberFormat;
+        set
+        {
+            if (numberFormat != value)
+                SetNumberFormat(value);
+        }
+    }
+
+    void OnEnable()
+    {
+        Input.backButtonLeavesApp = true;
+        const string outputElementName = "output";
+        const string inputElementName = "input";
+        const string buttonGridName = "button-grid";
+
+        outputEntries = new List<NumberEntry>();
+
+        VisualElement root = UIDocument.rootVisualElement;
+        this.input = root.Q<Label>(inputElementName) ?? throw new NullReferenceException($"{inputElementName} not found in the UIDocument.");
+
+        this.output = root.Q<MultiColumnListView>(outputElementName) ?? throw new NullReferenceException("output not found in the UIDocument.");
+
+        output.itemsSource = outputEntries;
+
+        static VisualElement makeCell() => new Label();
+        Debug.Assert(output.columns.Count == 3, $"Expected column count 3, but acutal was {output.columns.Count}");
+
+        for (int columnIndex = 0; columnIndex < 3; columnIndex++)
+        {
+            output.columns[columnIndex].makeCell = makeCell;
+          
+        }
+        output.columns[0].bindCell = (e, row) => (e as Label).text = outputEntries[row].ColumnData(0, NumberFormat);
+        output.columns[1].bindCell = (e, row) => (e as Label).text = outputEntries[row].ColumnData(1, NumberFormat);
+        output.columns[2].bindCell = (e, row) => (e as Label).text = outputEntries[row].ColumnData(2, NumberFormat);
+
+
+        output.makeNoneElement = () => new Label(""); //avoid message "List is empty"
+  
+        buttonGrid = root.Q<VisualElement>(buttonGridName) ?? throw new NullReferenceException($"{buttonGridName} not found in the UIDocument.");
+        
+        buttonGrid.RegisterCallback<ClickEvent>(OnButtonGridClick);
+        buttonGrid.RegisterCallback<GeometryChangedEvent>(OnButtonGridGeometryChanged);
+
+        foreach (Format format in Enum.GetValues(typeof(Format)))
+        {
+            numberFormatButtons[(int)format] = format switch
+            {
+                Format.Normal => buttonGrid.Q<Button>("button-format-normal"),
+                Format.Bin => buttonGrid.Q<Button>("button-format-bin"),
+                Format.BalBin => buttonGrid.Q<Button>("button-format-balbin"),
+                Format.RotationsBin => buttonGrid.Q<Button>("button-format-rotbin"),
+                Format.RotationsBalBin => buttonGrid.Q<Button>("button-format-rotbalbin"),
+                Format.Partition => buttonGrid.Q<Button>("button-format-partition"),
+                _ => throw new ArgumentOutOfRangeException(nameof(format), format, "Unknown format")
+            };
+            if (numberFormatButtons[(int)format] == null)
+                Debug.LogWarning($"Button for format {format} not found");
+        }
+        
+        SetNumberFormat(Format.Normal); //call method (instead of property) to force update of the buttons
+      
+        ClearInput();
+        ClearOutput();
+
+        RefreshOutput(true);
+
+    }
+
+    private void RefreshOutput(bool makeCompleteRebuild = false)
+    {
+       
+        if (makeCompleteRebuild)
+        {
+       
+            output.Rebuild();
+
+        }
+        else
+            output.RefreshItems();
+
+        if (output.itemsSource.Count > 0)
+        {
+            output.ScrollToItem(output.itemsSource.Count - 1);
+            AssertColumnWidths();
+        }
+    }
+
+
+
+    private string InputText
+    {
+        get => input.text;
+        set => this.input.text = value;
+    }
+
+    private bool OutputEmpty => outputEntries.Count == 0;
+    private bool InputEmpty => string.IsNullOrEmpty(InputText);
+
+    private int AvailableOperands => outputEntries.Count + (InputEmpty ? 0 : 1);
+
+
+    private void SetNumberFormat(Format format)
+    {
+        numberFormat = format;
+      
+        const string selectedClass = "selected";
+
+        for (int i = 0; i < numberFormatButtons.Length; i++)
+        {
+            Button button = numberFormatButtons[i];
+            if (i == (int)format)
+                button.AddToClassList(selectedClass);
+            else
+                button.RemoveFromClassList(selectedClass);
+        }
+
+        RefreshOutput();
+    }
+
+    void ClearInput() => InputText = string.Empty;
+
+    void ClearOutput()
+    {
+        outputEntries.Clear();
+        RefreshOutput();
+    }
+
+    void ClearLast()
+    {
+        if (OutputEmpty)
+            return;
+        outputEntries.RemoveAt(outputEntries.Count - 1);
+        RefreshOutput();
+    }
+
+
+    bool PushNumber()
+    {
+        if (InputEmpty)
+        {
+            if (outputEntries.Count == 0)
+                return true; //no number to push
+            outputEntries.Add(new NumberEntry(outputEntries[^1].Rational));
+            RefreshOutput();
+            return true;
+        }
+        
+        Rational rational = new Rational(InputText);
+        if (rational.IsInvalid)
+        {
+            Debug.LogWarning($"Failed to parse {InputText}");
+            return false;
+        }
+        NumberEntry entry = new NumberEntry(rational);
+        outputEntries.Add(entry);
+        ClearInput();
+        RefreshOutput();
+        return true;
+    }
+
+    private void OnButtonGridClick(ClickEvent evt)
+    {
+        if (evt.target is not Button button)
+            return;
+
+        // Check and number format buttons if they were clicked
+        int numberFormatButtonIndex = Array.IndexOf(numberFormatButtons, button);
+        if (numberFormatButtonIndex >= 0)
+        {
+            NumberFormat = (Format)numberFormatButtonIndex;
+            return;
+        }
+
+        const string buttonPrefix = "button-";
+
+        string buttonValue = button.name.Length > buttonPrefix.Length ? button.name[buttonPrefix.Length..] : button.name;
+
+        switch (buttonValue)
+        {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                InputText += buttonValue;
+                break;
+            case "enter":
+                PushNumber();
+                break;
+            case "back-drop":
+                if (InputEmpty)
+                    ClearLast();
+                else
+                    InputText = InputText.Remove(InputText.Length - 1);
+                break;
+            case "swap":
+                //implement this
+                break;
+            case "neg":
+                PerformUnaryOperation((a) => -a);        
+                break;
+            case "reciprocal":
+                PerformUnaryOperation((a) => a.Reciprocal);
+                break;
+            case "square":
+                PerformUnaryOperation((a) => a * a);
+                break;
+            case "sum":
+                PerformBinaryOperation((a, b) => a + b);
+                break;
+            case "diff":
+                PerformBinaryOperation((a, b) => a - b);
+                break;
+            case "prod":
+                PerformBinaryOperation((a, b) => a * b);
+                break;
+            case "quotient":
+                PerformBinaryOperation((a, b) => a / b);
+                break;
+            case "clear":
+                ClearInput();
+                ClearOutput();
+                break;
+            case "mod":
+                PerformBinaryOperation((a, b) => a % b);
+                break;
+            case "div-ones":
+                PerformUnaryOperation((a) => a.DivideByMersenneCeiling());
+                break;
+            default:
+                Debug.LogWarning($"Unhandled button: {button.name}");
+                break;
+        }
+
+    }
+
+
+
+
+    void PerformUnaryOperation(Func<Rational, Rational> operation)
+    {
+        if (AvailableOperands < 1)
+            return; //need a number to perform operation: abort operation
+
+        Rational operand;
+
+        if (InputEmpty)
+        {
+            operand = outputEntries[^1].Rational;
+        }
+        else
+        {
+            operand = new Rational(InputText);
+            if (operand.IsInvalid)
+            {
+                Debug.LogWarning($"Failed to parse {InputText}");
+                return;
+            }
+        }
+
+        Rational result = operation(operand);
+        if (result.IsInvalid)
+        {
+            Debug.LogWarning($"Invalid operation");
+            return;
+        }
+        
+        if (InputEmpty)
+            outputEntries.RemoveAt(outputEntries.Count - 1);
+        else
+            ClearInput();
+
+        outputEntries.Add(new NumberEntry(result));
+        RefreshOutput();
+
+
+    }
+
+    void PerformBinaryOperation(Func<Rational, Rational, Rational> operation)
+    {
+        if (AvailableOperands < 2)   
+            return; //need two numbers to perform operation: abort operation
+
+        Rational operandA, operandB;
+
+        if (InputEmpty)
+        {
+            operandA = outputEntries[^2].Rational;
+            operandB = outputEntries[^1].Rational;
+        }
+        else
+        {
+            operandA = outputEntries[^1].Rational;
+            operandB = new Rational(InputText);
+            if (operandB.IsInvalid)
+            {
+                Debug.LogWarning($"Failed to parse {InputText}");
+                return;
+            }
+        }
+
+        Rational result = operation(operandA, operandB);
+        if (result.IsInvalid)
+        {
+            Debug.LogWarning($"Invalid operation");
+            return;
+        }
+        outputEntries.RemoveAt(outputEntries.Count - 1);
+
+        if (InputEmpty)
+            outputEntries.RemoveAt(outputEntries.Count - 1);
+        else
+            ClearInput();
+
+        outputEntries.Add(new NumberEntry(result));
+        RefreshOutput();
+
+        
+    }
+
+    private void OnButtonGridGeometryChanged(GeometryChangedEvent evt)
+    {
+        var height = buttonGrid.resolvedStyle.height;
+        var width = buttonGrid.resolvedStyle.width;
+        float ratio = 4f;
+
+        if (height * ratio > width)
+        {
+            buttonGrid.style.height = width / ratio;
+        }
+
+    }
+
+
+
+    private void AssertColumnWidths()
+    {
+        if (outputEntries.Count == 0)
+            return;
+
+        for (int column = 0; column < output.columns.Count; column++)
+        {
+            int maxCharCount = 8;
+            for (int row = 0; row < outputEntries.Count; row++)
+            {
+                NumberEntry entry = outputEntries[row];
+                int charCount = entry.ColumnData(column, NumberFormat).Length;
+                if (charCount > maxCharCount)
+                {
+                    maxCharCount = charCount;
+                }
+            }
+            output.columns[column].width = maxCharCount * 24;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (buttonGrid != null)
+        {
+            buttonGrid.UnregisterCallback<GeometryChangedEvent>(OnButtonGridGeometryChanged);
+
+            buttonGrid.UnregisterCallback<ClickEvent>(OnButtonGridClick);
+        }
+    }
+}
