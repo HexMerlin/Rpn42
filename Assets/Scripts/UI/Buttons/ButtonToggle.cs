545d1f4b42d4f5cf1fa2d6cbb9472296df33e6b4
HexMerlin <alec-sweden@hotmail.com>
2024-10-01 15:56:43 +0200

    Updates

---
+++ Assets/Scripts/UI/Buttons/ButtonToggle.cs
@@ -0,0 +1,23 @@
+﻿using MathLib;
+using UnityButton = UnityEngine.UIElements.Button;
+
+public class ButtonToggle : AbstractButton
+{
+    //public int Digit { get; }
+
+    public ButtonToggle(UnityButton unityButton) : base(unityButton)
+    {
+        this.UnityButton.userData = "kalle";
+    }
+
+    public override void Execute(OperationController opc) => throw new System.NotImplementedException();
+    public override void UpdateEnabledStatus(OperationController opc, Q leftOperand, Q rightOperand) => throw new System.NotImplementedException();
+
+    //public override void UpdateEnabledStatus(OperationController opc, Q leftOperand, Q rightOperand)
+    //{
+    //    SetEnabled(Digit < opc.NumberBase);
+    //}
+
+    //public override void Execute(OperationController opc) => opc.PerformAddInput(Digit.ToString());
+}
+
